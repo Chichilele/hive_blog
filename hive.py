51277d0e197eba42a57baf5d7809b8d40fbbcfff
G <gauthier.castro@cfms.org.uk>
2021-07-05 15:56:22 +0100

    att function annotation to make_shell_context

--- hive.py
+++ hive.py
@@ -1,7 +1,8 @@
+from typing import Dict
 from app import flask_app, db
 from app.models import User, Post
 
 
 @flask_app.shell_context_processor
-def make_shell_context():
+def make_shell_context() -> Dict:
     return {"db": db, "User": User, "Post": Post}
