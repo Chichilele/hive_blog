5fe88983fcb2694f42f59485c431915c8ced2679
G <gauthier.castro@cfms.org.uk>
2021-07-05 13:56:23 +0100

    rename app to have and app to flask_app

--- hive.py
+++ hive.py
@@ -1,4 +1,4 @@
-from app import app
+from hive import flask_app
 
 # if __name__ == "__main__":
 #     app.run()
--- hive/routes.py
+++ hive/routes.py
@@ -1,10 +1,10 @@
-from app import app
+from hive import flask_app
 from flask import render_template, request, flash, url_for
-from app.forms import LoginForm
+from hive.forms import LoginForm
 
 
-@app.route("/")
-@app.route("/index")
+@flask_app.route("/")
+@flask_app.route("/index")
 def index():
     user = {"username": "G"}
     posts = [
@@ -14,7 +14,7 @@ def index():
     return render_template("index.html", title=request.endpoint, user=user, posts=posts)
 
 
-@app.route("/login", methods=["GET", "POST"])
+@flask_app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
