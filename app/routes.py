8e7d88d69a6421949d4fcac9654fc30f4057facd
G <gauthier.castro@cfms.org.uk>
2021-07-05 13:51:29 +0100

    add login route

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, flash, url_for
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -11,3 +12,14 @@ def index():
         {"author": {"username": "HM Anibal Michael"}, "body": "Release day!"},
     ]
     return render_template("index.html", title=request.endpoint, user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user: {form.username.data}, remember_me={form.remember_me.data}"
+        )
+        return url_for("index")
+    return render_template("login.html", title="Sign In", form=form)
