00cde7ab39464895f60454f48c9732c524082513
G <gauthier.castro@cfms.org.uk>
2021-07-26 11:19:41 +0100

    add password hashing methods to User model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from app import db
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +13,25 @@ class User(db.Model):
 
     def __repr__(self) -> str:
         return f"<User='{self.username}', id='{self.id}'>"
+
+    def set_password(self, password: str) -> None:
+        """Hash and set user's password.
+
+        Args:
+            password (str): plain password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        """Check user password against hash.
+
+        Args:
+            password (str): plain password to be tested.
+
+        Returns:
+            bool: True if password matches, false otherwise.
+        """
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
