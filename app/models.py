e860aee628e246ffe3d736708f1e5a4085c03e4e
G <gauthier.castro@cfms.org.uk>
2021-07-26 12:01:01 +0100

    add user_loader

--- app/models.py
+++ app/models.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 from app import db
+from app import login
 
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
     posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __repr__(self) -> str:
@@ -42,3 +45,16 @@ class Post(db.Model):
 
     def __repr__(self) -> str:
         return f"<Post='{self.body}'>"
+
+
+@login.user_loader
+def load_user(id: int) -> User:
+    """User loader for flask_login.
+
+    Args:
+        id (int): User primary id.
+
+    Returns:
+        User: User instance.
+    """
+    return User.query.get(id)
