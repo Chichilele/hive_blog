b5b4109f0e1b8013ffa92b393560cba34b5e6e3d
G <gauthier.castro@cfms.org.uk>
2021-07-26 16:32:30 +0100

    init login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from config import Config
-from flask import Flask, app
+from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +8,6 @@ flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
 db = SQLAlchemy(flask_app)
 migrate = Migrate(flask_app, db)
-login = LoginManager(app)
+login = LoginManager(flask_app)
 
 from app import models, routes
