5f819770478a56a4ae960805bf780df2544c950a
G <gauthier.castro@cfms.org.uk>
2021-07-27 11:10:46 +0100

    update login logic and add logout route

--- app/routes.py
+++ app/routes.py
@@ -2,25 +2,30 @@ from typing import Union
 
 from flask import flash, redirect, render_template, request, url_for
 from flask.wrappers import Response
+from flask_login import current_user, login_user, login_required, logout_user
 
 from app import flask_app
 from app.forms import LoginForm
+from app.models import User
 
 
 @flask_app.route("/")
 @flask_app.route("/index")
+@login_required
 def index() -> str:
     """Index view
 
     Returns:
         str: rendered index.html template
     """
-    user = {"username": "G"}
+    # user = {"username": "G"}
     posts = [
         {"author": {"username": "Magic E"}, "body": "Gig review!"},
         {"author": {"username": "HM Anibal Michael"}, "body": "Release day!"},
     ]
-    return render_template("index.html", title=request.endpoint, user=user, posts=posts)
+    return render_template(
+        "index.html", title=request.endpoint, user=current_user.username, posts=posts
+    )
 
 
 @flask_app.route("/login", methods=["GET", "POST"])
@@ -28,12 +33,32 @@ def login() -> Union[str, Response]:
     """Login view and controller. Logs in the user or returns the index view if already logged in.
 
     Returns:
-        Union[str, Response]: index view if logged in, login view otherwise
+        Union[str, Response]: index view if logged in, login view otherwise.
     """
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(
-            f"Login requested for user: {form.username.data}, remember_me={form.remember_me.data}"
-        )
+    if current_user.is_authenticated:
         return redirect(url_for("index"))
-    return render_template("login.html", title="Sign In", form=form)
+    form = LoginForm()
+
+    ## POST request
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("index"))
+    ## GET request
+    else:
+        return render_template("login.html", title="Sign In", form=form)
+
+
+@flask_app.route("/logout")
+def logout() -> Response:
+    """Logout the user and redirects to index.
+
+    Returns:
+        Response: redirection to index.
+    """
+    logout_user()
+    return redirect(url_for("index"))
