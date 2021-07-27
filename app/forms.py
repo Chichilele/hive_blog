637fd26843c30ad0dfff6a90bbc3e7ccabf08066
G <gauthier.castro@cfms.org.uk>
2021-07-27 11:56:51 +0100

    implement RegistrationForm class

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,39 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = StringField("Password", validators=[DataRequired()])
+    password2 = StringField(
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    sumbit = SubmitField("Register")
+
+    def validate_username(self, username: str):
+        """username custom validator.
+
+        Args:
+            username (str): form's username.
+
+        Raises:
+            ValidationError: username already taken.
+        """
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Username taken, please use a different one.")
+
+    def validate_email(self, email: str):
+        """email custom validator.
+
+        Args:
+            email (str): form's email address.
+
+        Raises:
+            ValidationError: Email address already taken.
+        """
+        user = User.query.filter_by(username=email.data).first()
+        if user is not None:
+            raise ValidationError("Email address taken, please use a different one.")
