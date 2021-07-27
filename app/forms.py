26cc2ce9c4d8d3c1b16d81d1219e243f1e218fa3
G <gauthier.castro@cfms.org.uk>
2021-07-27 16:50:46 +0100

    implement class EditProfileForm

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.fields.simple import TextAreaField
+from wtforms.validators import Length, ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
 
@@ -45,3 +46,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=email.data).first()
         if user is not None:
             raise ValidationError("Email address taken, please use a different one.")
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
